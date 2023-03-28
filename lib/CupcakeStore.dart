df94fb382717d5ad59fc7c699a24e5d3658d1cb5
TomasWard <tomas@familiaward.com.ar>
2023-03-27 23:59:07 -0300

    todo: connect and display all variables

--- lib/CupcakeStore.dart
+++ lib/CupcakeStore.dart
@@ -8,6 +8,43 @@ class CupcakeStore extends GetView<ContractLinker> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
-  } //
+    return SafeArea(
+      child: Scaffold(
+          body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(children: [
+          Image.asset('assets/cupcake.png', width: 100, height: 100),
+          const Text('Cupcake ETH', style: TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold)),
+          const Spacer(),
+          Row(
+            children: [
+              const Spacer(),
+              actionButton('Buy From Machine', () {
+                print('Buy From Machine');
+              }),
+              const Spacer(),
+              actionButton('Buy From User', () {
+                print('Buy From User');
+              }),
+              const Spacer(),
+            ],
+          )
+        ]),
+      )),
+    );
+  }
+
+  Widget actionButton(String title, Function() action) {
+    return ElevatedButton(
+      onPressed: action,
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        backgroundColor: Colors.pinkAccent.shade100,
+      ),
+      child: Text(
+        title,
+        style: const TextStyle(color: Colors.black),
+      ),
+    );
+  }
 }
